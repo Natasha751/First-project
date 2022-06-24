0f2f8f6ce7983b3daac0ea0ab035b7e0b5ff19a2
natasha751 <ideveloper93@gmail.com>
2022-06-24 15:14:08 +0530

    test

--- Betfamre/opertor/Array/1.js
+++ Betfamre/opertor/Array/1.js
@@ -3,3 +3,13 @@ for(i=0;i<=a.length-1;i++)
 {console.log(a[i])
 }
 console.log(a[a.length-1])
+let x=a.push("hat");
+console.log(x);
+console.log(a);
+let y= a.pop();
+console.log(a);
+let t= a.shift("fry");
+let b= a.unshift("yty");
+console.log(a);
+
+
