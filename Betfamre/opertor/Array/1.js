7a579e2136b8c992b5b5d22fa8ea1cf2b6e90fd4
natasha751 <ideveloper93@gmail.com>
2022-06-27 14:12:01 +0530

    test

--- Betfamre/opertor/Array/1.js
+++ Betfamre/opertor/Array/1.js
@@ -1,15 +1,15 @@
 let a=[1,2,3,4];//creating array
-for(i=0;i<=a.length-1;i++)
+for(i=0;i<=a.length-1;i++)//accessing all item using for loop
 {console.log(a[i])
 }
-console.log(a[a.length-1])
-let x=a.push("hat");
+console.log(a[a.length-1])//accessing last element
+let x=a.push("hat");//push method
 console.log(x);
 console.log(a);
-let y= a.pop();
+let y= a.pop();//pop method
 console.log(a);
-let t= a.shift("fry");
-let b= a.unshift("yty");
+let t= a.shift("fry");//shift method
+let b= a.unshift("yty");//unshift method
 console.log(a);
 
 
---
+++ Betfamre/opertor/Array/find.js
@@ -0,0 +1,6 @@
+
+const ages = [3, 10, 18, 20];
+
+function checkAge(age) {
+  return age > 18;
+}
