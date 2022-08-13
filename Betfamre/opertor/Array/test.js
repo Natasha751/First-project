564bdcd54026ff5d7df2b28b3f58db173198876c
Natasha751 <ideveloper93@gmail.com>
2022-08-13 07:41:38 +0530

    hello

---
+++ Betfamre/opertor/Array/test.js
@@ -0,0 +1,8 @@
+let employee = [emp={
+    id: 101,
+    name: "Rahul Gandhi"
+    }
+    ,
+ramya_2 ={id:105}, ramya_3={ty:090}//create array of objects
+]
+console.log (employee)//access array of object
