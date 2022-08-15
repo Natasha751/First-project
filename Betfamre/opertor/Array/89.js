3c3e57b30e9489f61f6622c3ab36b4a319cdcd2d
Natasha751 <ideveloper93@gmail.com>
2022-08-16 03:08:07 +0530

    okie

---
+++ Betfamre/opertor/Array/89.js
@@ -0,0 +1,17 @@
+let activities = [
+    ['Work', 9],
+    ['Eat', 1],
+    ['Commute', 2],
+    ['Play Game', 1],
+    ['Sleep', 7]
+];
+console.table(activities);//creating 2-D array
+
+for (let i = 0; i < activities.length; i++) {
+    // get the size of the inner array
+    var innerArrayLength = activities[i].length;
+    // loop the inner array
+    for (let j = 0; j < innerArrayLength; j++) {
+        console.log('[' + i + ',' + j + '] = ' + activities[i][j]);
+    }
+}
